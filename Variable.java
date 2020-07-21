cad6514ce26c08ee357a98e7385e64c3591fff78
kimdaye77 <63107805+kimdaye77@users.noreply.github.com>
2020-07-21 22:49:01 +0900

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,57 @@
+package Study;
+import java.util.Scanner;
+
+public class Variable {
+   
+   //Q1 : 자바 내 8개의 기본형 변수들을 선언하고, 오버플로가 나지 않는 수로 초기화하세요.(어떤 수도 됩니다.)
+   //또한, 문자형-정수형, 정수형-정수형, 정수형-실수형, 실수형-실수형에서 자동 형변환의 되는 사례를 보여주세요.(되기만 하면 됩니다.)
+   public static void Q1() {
+      //여기에 코드를 작성하세요.
+	   boolean t;
+	   char a;
+	   byte b;
+	   short c;
+	   int d;
+	   long e;
+	   float f;
+	   double g;
+	   t= true;
+	   a = 'b';
+	   b = 1;
+	   c = 1;
+	   d = 30;
+	   e = 100000000000L;
+	   f = 1;
+	   g = 40.52;
+	   int i = a;
+	   long h = d;
+	   float p = b;
+	   System.out.println(i);
+	   System.out.println(h);
+	   System.out.println(p);
+      //여기까지 출력되면 문제가 없는 겁니다.
+      System.out.println("Not Problem Found.");
+   }
+   
+   //Q2 : 1개의 숫자를 입력하면, 그에 대응하는 ASCII 문자열을 출력하는 메소드를 작성하세요.(아직 별도의 예외 처리는 필요 없습니다.)
+   //힌트 : char 형변환을 이용하세요.
+   public static void Q2() {
+
+      //여기에 코드를 작성하세요.
+	  Scanner scanner = new Scanner(System.in);
+      int n = scanner.nextInt();
+      char c= (char)n;
+      System.out.println(c);
+      
+   }
+
+   //main method
+   public static void main(String[] args) {
+      System.out.println("Q1 started.");
+      Q1();
+      System.out.println("Q2 started.");
+      Q2();
+
+   }
+
+}
